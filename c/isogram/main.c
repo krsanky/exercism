c25be8d8ece2f5279ed6a276440009daeac37867
paul wisehart <paul@oldcode.org>
2019-10-01 03:29:30 -0400

    asd

---
+++ c/isogram/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "src/isogram.h"
+
+int
+main()
+{
+
+
+	int i1 = is_isogram("awdAsb");
+	printf("i1:%d\n", i1);
+
+
+	return 0;
+}
--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,12 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+
 #include "isogram.h"
-#include "string.h"
 
 bool
-is_isogram(const char phrase[])
+is_isogram(const char *phrase)
 {
-	int length = strlen(phrase);
+	int		length;
+	int		i;
+/*	int		letter_counts[26];*/
+	char	*phr;
+
+	length = strlen(phrase);
+	assert((phr = malloc(length+1)) != NULL);
+
+	for (i=0; i<length; i++) {
+		printf("[%d]", phrase[i]);
+		phr[i] = toupper(phrase[i]);
+	}
+	printf("\n");
+	printf("phr:%s\n", phr);
+
 	if (length > 100)
 		return 1;
+
+	printf("A:%d Z:%d\n", 'A', 'Z');
+	free(phr);
 	return 0;
 }
-
