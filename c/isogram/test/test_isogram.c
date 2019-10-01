0ad8ba85b116f44385804797d87f439ed1363e94
paul wisehart <paul@oldcode.org>
2019-10-01 04:19:15 -0400

    works

--- c/isogram/test/test_isogram.c
+++ c/isogram/test/test_isogram.c
@@ -42,49 +42,41 @@ static void test_longest_known_isogram(void)
 
 static void test_duplicated_letter_mixed_case(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("Alphabet"));
 }
 
 static void test_duplicated_letter_mixed_case_lowercase_first(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("alphAbet"));
 }
 
 static void test_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("thumbscrew-japingly"));
 }
 
 static void test_duplicated_letter_following_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("thumbscrew-jappingly"));
 }
 
 static void test_duplicated_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("six-year-old"));
 }
 
 static void test_multiple_whitespace(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("Emily Jung Schwartzkopf"));
 }
 
 static void test_duplicated_letter_within_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("accentor"));
 }
 
 static void test_same_first_and_last_characters(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("angola"));
 }
 
