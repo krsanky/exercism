17136c03b65b989fa876f2a57f82db45fadd3c11
paul wisehart <paul@oldcode.org>
2019-10-01 11:56:29 -0400

    go mod init <...>
    Don't know how to not use modules?

---
+++ go/hamming/cmd/hm/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/krsanky/exercism/go/hamming"
+)
+
+func main() {
+	fmt.Printf("hw\n")
+	_, _ = hamming.Distance("abc", "bbc")
+}
---
+++ go/hamming/go.mod
@@ -0,0 +1,3 @@
+module github.com/krsanky/exercism/go/hamming
+
+go 1.12
