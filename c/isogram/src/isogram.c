20c887585ec41a4ac16643d765957797c0448711
paul wisehart <paul@oldcode.org>
2019-10-01 04:33:53 -0400

    done again

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -7,22 +7,23 @@
 bool
 is_isogram(const char *phrase)
 {
-	unsigned long		i;
-	int		letter_counts[26] = {0}; 
-	char	u;
+	unsigned long	i;
+	int		letter_counts[26] = {0};
+	char		u;
 
 	if (phrase == NULL)
-		return 0;
+		return false;
 	if (strlen(phrase) == 0)
-		return 1;
+		return true;
+
 	for (i = 0; i < strlen(phrase); i++) {
 		u = toupper(phrase[i]);
 		if ((u >= 65) && (u <= 90)) {
-			if (letter_counts[u - 65] > 0) 
-				return 0;
+			if (letter_counts[u - 65] > 0)
+				return false;
 			letter_counts[u - 65]++;
 		}
 	}
 
-	return 1;
+	return true;
 }
