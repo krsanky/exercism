9491cceb481143527a5d4b1aadd492deec6e04d2
paul wisehart <paul@oldcode.org>
2019-10-01 16:04:17 -0400

    asd

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,22 +1,15 @@
 package hamming
 
-func Max(x, y int) int {
-    if x < y {
-        return y
-    }
-    return x
-}
+import "fmt"
 
 func Distance(a, b string) (int, error) {
-	var count int = 0
-	var length int = Max(len(a), len(b))
+	if len(a) != len(b) {
+		return -1, fmt.Errorf("len(a) != len(b)")
+	}
 
-	for i := 0; i < length; i++ {
-		if (i < len(a)) && (i < len(b)) {
-			if a[i] != b[i] {
-				count++
-			}
-		} else {
+	var count int = 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
 			count++
 		}
 	}
