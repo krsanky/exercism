8a01230098c8f0ab0cb6ea97eb531d9cbbe4d4b1
paul wisehart <paul@oldcode.org>
2019-10-20 11:13:06 -0400

    add var to hold strlen(p)

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -10,13 +10,15 @@ is_isogram(const char *phrase)
 	unsigned long	i;
 	int		letter_counts[26] = {0};
 	char		u;
+	unsigned long	phrasel;
 
 	if (phrase == NULL)
 		return false;
-	if (strlen(phrase) == 0)
+	phrasel = strlen(phrase);
+	if (phrasel == 0)
 		return true;
 
-	for (i = 0; i < strlen(phrase); i++) {
+	for (i = 0; i < phrasel; i++) {
 		u = toupper(phrase[i]);
 		if ((u >= 65) && (u <= 90)) {
 			if (letter_counts[u - 65] > 0)
