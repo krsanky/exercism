2e9f9a950f8d81f574fa30185fa83a86f98d114d
paul wisehart <paul@oldcode.org>
2019-10-20 06:14:25 -0400

    compiles

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -1,1 +1,13 @@
+#include <string.h>
+
 #include "word_count.h"
+
+int
+word_count(const char *input_text, word_count_word_t * words)
+{
+	unsigned long	l;
+	l = strlen(input_text);	
+	l = sizeof(words);
+	return 0;
+}
+
