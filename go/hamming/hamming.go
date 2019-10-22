50afad43fb95b60ed76864e21885e257c0b70d2c
paul wisehart <paul@oldcode.org>
2019-10-22 07:54:43 -0400

    update with reccomendations

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -3,13 +3,14 @@ package hamming
 import "fmt"
 
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
-		return -1, fmt.Errorf("len(a) != len(b)")
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
+		return 0, fmt.Errorf("len(a) != len(b)")
 	}
 
 	var count int = 0
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	for i := 0; i < len(ar); i++ {
+		if ar[i] != br[i] {
 			count++
 		}
 	}
