961acc35a1eaaf6db94296c93a0650304c81114c
paul wisehart <paul@oldcode.org>
2019-10-22 14:50:19 -0400

    compiles

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,12 @@
+#include <sys/types.h>
+#include <stdio.h>
+
 #include "resistor_color_duo.h"
+
+uint16_t
+color_code(resistor_band_t bands[])
+{
+	printf("sizeof bands:%lu\n", sizeof(&bands));
+	return 0;
+}
+
--- c/resistor-color-duo/src/resistor_color_duo.h
+++ c/resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,19 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
 
+typedef enum {
+	BLACK,
+	BROWN,
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	BLUE,
+	VIOLET,
+	GREY,
+	WHITE
+}		resistor_band_t;
+
+uint16_t 	color_code(resistor_band_t[]);
+
 #endif
