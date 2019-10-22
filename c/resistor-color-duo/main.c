f2419e78eae4bb15f8395639ace5cc28480daa01
paul wisehart <paul@oldcode.org>
2019-10-22 15:36:42 -0400

    asd

---
+++ c/resistor-color-duo/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "src/resistor_color_duo.h"
+
+int
+main()
+{
+	printf("asd\n");
+
+	return 0;
+}
--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -6,6 +6,11 @@
 uint16_t
 color_code(resistor_band_t bands[])
 {
+/*
+   uint16_t actual = color_code((resistor_band_t[]){ BROWN, BLACK });
+   TEST_ASSERT_EQUAL_UINT16(10, actual);
+*/
+	
 	printf("sizeof bands:%lu\n", sizeof(&bands));
 	return 0;
 }
