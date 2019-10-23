c3bfec09eccc4c9e279ef9828ca681b8a40f5347
paul wisehart <paul@oldcode.org>
2019-10-23 02:47:07 -0400

    pull in code from duo

--- c/resistor-color-trio/src/resistor_color_trio.c
+++ c/resistor-color-trio/src/resistor_color_trio.c
@@ -1,1 +1,10 @@
+#include <sys/types.h>
+#include <stdio.h>
+
 #include "resistor_color_trio.h"
+
+uint16_t
+color_code(resistor_band_t bands[])
+{
+	return (uint16_t) bands[0] * 10 + (uint16_t) bands[1];
+}
