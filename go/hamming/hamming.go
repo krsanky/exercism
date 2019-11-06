9862b2ff79b4a4d4c47c7f655887e2418e928c5a
paul wisehart <paul@oldcode.org>
2019-11-06 09:26:04 -0500

    asd

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,15 +1,15 @@
 package hamming
 
-import "fmt"
+import "errors"
 
 func Distance(a, b string) (int, error) {
 	ar, br := []rune(a), []rune(b)
 	if len(ar) != len(br) {
-		return 0, fmt.Errorf("The lengths of a and b differ.")
+		return 0, errors.New("the lengths of a and b differ")
 	}
 
 	var count int = 0
-	for i := 0; i < len(ar); i++ {
+	for i := range ar {
 		if ar[i] != br[i] {
 			count++
 		}
