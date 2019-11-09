765934a774bf08d2909b3c0535bdf6846b2ae2c9
paul wisehart <paul@oldcode.org>
2019-11-09 00:02:18 -0500

    go/raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -3,5 +3,25 @@ package raindrops
 import "fmt"
 
 func Convert(n int) string {
-	return fmt.Sprintf("%d", n)
+	var res string
+	var has bool
+
+	if n%3 == 0 {
+		res += "Pling"
+		has = true
+	}
+	if n%5 == 0 {
+		res += "Plang"
+		has = true
+	}
+	if n%7 == 0 {
+		res += "Plong"
+		has = true
+	}
+
+	if !has {
+		res = fmt.Sprintf("%d", n)
+	}
+
+	return res
 }
